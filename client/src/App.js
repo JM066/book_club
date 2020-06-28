7dc94a0750fd4b3f0eb9f136ef9b23498d87d577
Albane Chepeau <you@example.com>
2020-06-28 15:11:17 +0100

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
 import Filter from "./components/filter"
-import Carousel from "./components/carousel"
-
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-
 //import Overlay from "./components/overlay"
 import './App.css';
 
@@ -11,7 +7,9 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books:[]
+      books:[],
+      bookdetails: false,
+      showBook: []
     };
   }
 
@@ -27,22 +25,91 @@ export default class App extends Component {
       });
   };
 
+  getBookdetails = (id) => {
+    const data = {books : id};
+    fetch(`/books/${id}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ 
+          showBook: response });
+      });
+  };
+
+  handleClick(e){
+    // e.preventdefault();
+    this.setState({
+      bookdetails: !this.state.bookdetails
+    });           
+  }
+
   renderLibrary() {
   return this.state.books.map((books,id) => {
     return(
       <li key={id} className="book-list">
         <span onClick ={() => this.getBookclub(books.id)}>
-          <div className="book-cover-container">
-          <img src={books.cover} alt={books.title} className="book-cover"/>                    
+          <div className="book-cover">
+          <img onClick={(e) => this.handleClick()} src={books.cover} alt={books.title} className="book-cover"/>                    
           </div>
-          <h4>{books.title}</h4>
-          <p>{books.author}</p>
+          <div className={this.state.bookdetails ? "bookdetails" : "bookhidden"}>
+
+
+
+
+
+          {/* <h4>{books.title}</h4>
+          <h5>{books.author}</h5>
+          <p>{books.summary}</p> */}
+          </div>
           <br></br>
           <br></br>
         </span>
       </li>
     )
   })}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
   filterBook(filteredList){
     this.setState({
@@ -54,65 +121,9 @@ export default class App extends Component {
     return (
       <div>
         <h1>London Book Club</h1>
-
-        <Carousel />
-
-
-        {/* <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="" alt="First slide"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>title</h5>
-          <p>summary</p>
-        </div>
-        </div>
-
-        <div class="carousel-item">
-          <img class="d-block w-100" src="" alt="Second slide"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>title</h5>
-          <p>summary</p>
-        </div>
-        </div>
-    
-        <div class="carousel-item">
-          <img class="d-block w-100" src="" alt="Third slide"/>
-        <div class="carousel-caption d-none d-md-block">
-          <h5>title</h5>
-        <p>summary</p>
-        </div>
-        </div>
-
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-
-        </div>
-        </div> */}
-
-
-
-
-
-
-
         <div>
           <Filter filterBook={filteredList => this.filterBook(filteredList)}/>
-          {/* <Overlay /> */}
         </div>
-        
         <ul>
           <div className="all-books">
             {this.renderLibrary()}
---
+++ client/src/components/slider.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+// import 'pure-react-carousel/dist/react-carousel.es.css';
+
+// export default class Carousel extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//         };
+//       }
+
+//   render() {
+//     return (
+
+//     <div>
+       
+//       <CarouselProvider naturalSlideWidth={70} naturalSlideHeight={30} totalSlides={3}>
+//     <Slider>
+//         <Slide index={0}><img className="slide-one" src="https://images-na.ssl-images-amazon.com/images/I/41+M6sayf6L._SX309_BO1,204,203,200_.jpg"/></Slide>
+//         <Slide index={1}><img className="slide-two" src="https://images-na.ssl-images-amazon.com/images/I/51mN3bY0JjL._SX332_BO1,204,203,200_.jpg"/></Slide>
+//         <Slide index={2}><img className="slide-three" src="https://images-na.ssl-images-amazon.com/images/I/51jXWPV1PoL._SX316_BO1,204,203,200_.jpg"/></Slide>
+//     </Slider>
+//     <ButtonBack>Back</ButtonBack>
+//     <ButtonNext>Next</ButtonNext>
+
+
+
+
+//       </CarouselProvider>
+
+//     </div>
+//     );
+//   }
+// }
